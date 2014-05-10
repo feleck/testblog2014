d84b656eb6303d12a479c5369483fdadee470550
Marcin Felczak <feleck@gmail.com>
2014-05-11 00:45:43 +0200

    modification of comment model  * added references to user and post  * added field body modification in voto model to abusive mark

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,13 @@
 class Comment
+  include Mongoid::Document
+  include Mongoid::Timestamps
+
+  field :body, type: String
+  field :abusive, type: Boolean, default: false
+  
+  validates :body, presence: true
+  
+  belongs_to :user
+  belongs_to :post
+  has_many :votes
 end
--- app/models/vote.rb
+++ app/models/vote.rb
@@ -2,9 +2,15 @@ class Vote
   include Mongoid::Document
   include Mongoid::Timestamps
   
+  after_create :mark_abusive
+
   field :value, type: Integer, default: 0
 
   belongs_to :user
   belongs_to :comment
 
+  protected 
+  def mark_abusive
+   comment.update_attribute :abusive, true if comment.votes.where(value: -1).count >= 3
+  end
 end
