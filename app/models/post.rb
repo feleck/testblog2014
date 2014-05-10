b9317312573226c0ce95d455be77644ed44761f3
Marcin Felczak <feleck@gmail.com>
2014-05-11 00:51:14 +0200

    modification of post model  * added comments relation  * added hotness

--- app/models/post.rb
+++ app/models/post.rb
@@ -10,11 +10,28 @@ class Post
   validates_presence_of :body, :title
 
   belongs_to :user
-
+  has_many :comments, dependent: :destroy
+  
   default_scope ->{ ne(archived: true) }
 
   def archive!
     update_attribute :archived, true
   end
 
+  def hotness
+    hotness = 3
+    hours_ago = (Time.now - self.created_at) / 3600
+    case hours_ago
+    when (0..24)
+      hotness = 3
+    when (24..72)
+      hotness = 2
+    when (24*3..24*7)
+      hotness = 1
+     else
+      hotness = 0
+    end
+    hotness += 1 if comments.count >= 3
+    hotness
+  end
 end
